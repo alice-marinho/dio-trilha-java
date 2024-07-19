0ab497aa4946f89d39e596ca213e1ae6e0f241c0
alice <alicemarinhogms@gmail.com>
2024-07-18 23:24:45 -0300

    Aprendendo sobre tipos de operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 9;
+        System.out.println(numero);
+
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
