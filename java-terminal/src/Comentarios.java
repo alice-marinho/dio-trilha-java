bc3e700afe53339fc29a52fc2d5117446f9edec3
alice <alicemarinhogms@gmail.com>
2024-07-26 12:24:11 -0300

    Add novos exercícios

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,8 @@
+public class Comentarios {
+    public static void main(String[] args) throws Exception {
+       
+        System.out.println("Oi Turma, fui executado pelo Terminal");
+
+    }
+
+}
